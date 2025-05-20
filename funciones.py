bacc5b73a1c2dcb2655db1711d956f7b07acb9e0
johan <johanbadillomj@gmail.com>
2025-05-20 12:14:07 -0500

    fix: :construction: creation of features and improvements

--- funciones.py
+++ funciones.py
@@ -1,5 +1,195 @@
 from datetime import *
 from tabulate import *
+totalGastosComida=0
+totalGastosTransporte=0
+totalGastosEntretenimiento=0
+totalGastosSalud=0
+totalGastosRopa=0
+totalGastosTecnologia=0
+totalGastosHogar=0
+totalGastosOtros=0
+
+
+
+def mensaje1():
+    print("=============================================\
+            \n            Registrar Nuevo Gasto\
+        \n=============================================\
+        \nIngrese la informacion del gasto:\
+        \n")
+
+def mensajeIni():
+    print("=============================================\
+    \n         Simulador de Gasto Diario\
+    \n=============================================\
+    \nSeleccione una opción:\
+    \n\
+    \n1. Registrar nuevo gasto\
+    \n2. Enumere los gastos\
+    \n3. Calcular total de gastos\
+    \n4. Generar reporte de gastos\
+    \n5. Salir\
+    \n=============================================")
+
+def mensaje2():
+    print("=============================================\
+            \n                Listar Gastos\
+            \n=============================================\
+            \nSeleccione una opción para filtrar los gastos:\
+            \n""\
+            \n1. Ver todos los gastos\
+            \n2. Filtrar por categoria\
+            \n3. Filtrar por rango de fechas\
+            \n4. Regresar al menú principal\
+            \n=============================================")
+    
+def filtroCate(confirmacion,listaGastos):
+    print("=============================================")
+        
+    if (confirmacion==1):
+        print(tabulate(listaGastos, tablefmt="double_outline"))
+    elif (confirmacion==2):
+        categoria=int(input("1. Comida\n2. Transporte\n3. Entretenimiento\n4. Salud\n5. Ropa\n6. Tecnologia\n7. Hogar\n8. Otros\n=============================================\n"))
+        if categoria == 1:
+            campos = ["fecha", "montoGasto", "categoria","cantidad"]
+            listaComida = []
+            for i in range(len(listaGastos)):
+                if listaGastos[i]["categoria"] == "comida":
+                    gasto = listaGastos[i]
+                    gasto_filtrado = {k: gasto[k] for k in campos}
+                    listaComida.append(gasto_filtrado)
+            if listaComida:
+                print(tabulate(listaComida, headers="keys", tablefmt="pipe"))
+            else:
+                print("No hay gastos registrados para la clase de comida")
+        elif(categoria==2):
+            campos = ["fecha", "montoGasto", "categoria","cantidad"]
+            listaTransporte = []
+            for i in range(len(listaGastos)):
+                if listaGastos[i]["categoria"] == "transporte":
+                    gasto = listaGastos[i]
+                    gasto_filtrado = {k: gasto[k] for k in campos}
+                    listaTransporte.append(gasto_filtrado)
+            if listaTransporte:
+                print(tabulate(listaTransporte, headers="keys", tablefmt="pipe"))
+            else:
+                print("No hay gastos registrados para la clase de transporte")
+        elif(categoria==3):
+            campos = ["fecha", "montoGasto", "categoria","cantidad"]
+            listaEntretenimiento = []
+            for i in range(len(listaGastos)):
+                if listaGastos[i]["categoria"] == "entretenimiento":
+                    gasto = listaGastos[i]
+                    gasto_filtrado = {k: gasto[k] for k in campos}
+                    listaEntretenimiento.append(gasto_filtrado)
+            if listaEntretenimiento:
+                print(tabulate(listaEntretenimiento, headers="keys", tablefmt="pipe"))
+            else:
+                print("No hay gastos registrados para la clase de entretenimiento")
+        elif(categoria==4):
+            campos = ["fecha", "montoGasto", "categoria","cantidad"]
+            listaSalud = []
+            for i in range(len(listaGastos)):
+                if listaGastos[i]["categoria"] == "salud":
+                    gasto = listaGastos[i]
+                    gasto_filtrado = {k: gasto[k] for k in campos}
+                    listaSalud.append(gasto_filtrado)
+            if listaSalud:
+                print(tabulate(listaSalud, headers="keys", tablefmt="pipe"))
+            else:
+                print("No hay gastos registrados para la clase de salud")
+        elif(categoria==5):
+            campos = ["fecha", "montoGasto", "categoria","cantidad"]
+            listaRopa = []
+            for i in range(len(listaGastos)):
+                if listaGastos[i]["categoria"] == "ropa":
+                    gasto = listaGastos[i]
+                    gasto_filtrado = {k: gasto[k] for k in campos}
+                    listaRopa.append(gasto_filtrado)
+            if listaRopa:
+                print(tabulate(listaRopa, headers="keys", tablefmt="pipe"))
+            else:
+                print("No hay gastos registrados para la clase de ropa")
+        elif(categoria==6):
+            campos = ["fecha", "montoGasto", "categoria","cantidad"]
+            listaTecnología = []
+            for i in range(len(listaGastos)):
+                if listaGastos[i]["categoria"] == "tecnología":
+                    gasto = listaGastos[i]
+                    gasto_filtrado = {k: gasto[k] for k in campos}
+                    listaTecnología.append(gasto_filtrado)
+            if listaTecnología:
+                print(tabulate(listaTecnología, headers="keys", tablefmt="pipe"))
+            else:
+                print("No hay gastos registrados para la clase de tecnología")
+        elif(categoria==7):
+            campos = ["fecha", "montoGasto", "categoria","cantidad"]
+            listaHogar = []
+            for i in range(len(listaGastos)):
+                if listaGastos[i]["categoria"] == "hogar":
+                    gasto = listaGastos[i]
+                    gasto_filtrado = {k: gasto[k] for k in campos}
+                    listaHogar.append(gasto_filtrado)
+            if listaHogar:
+                print(tabulate(listaHogar, headers="keys", tablefmt="pipe"))
+            else:
+                print("No hay gastos registrados para la clase de hogar")
+        elif(categoria==8):
+            campos = ["fecha", "montoGasto", "categoria","cantidad"]
+            listaOtros = []
+            for i in range(len(listaGastos)):
+                if listaGastos[i]["categoria"] == "otros":
+                    gasto = listaGastos[i]
+                    gasto_filtrado = {k: gasto[k] for k in campos}
+                    listaOtros.append(gasto_filtrado)
+            if listaOtros:
+                print(tabulate(listaOtros, headers="keys", tablefmt="pipe"))
+            else:
+                print("No hay gastos registrados para la clase de comida")
+        else:
+            print("=============================================\nNo se encontro opcion valida \nRegresando al menu principal")
+    elif(confirmacion==3):
+        campos=["fecha", "montoGasto", "categoria","descripcion","cantidad"]
+        listaFecharango=[]
+        fechaInicio=input("Ingrese la fecha inicial de la busqueda(formato YYYY-MM-DD): ")
+        fechaInicio = datetime.strptime(fechaInicio, "%Y-%m-%d").date()      
+        fechaFinal=input("Ingrese la fecha final de la busqueda (formato YYYY-MM-DD): ")
+        fechaFinal = datetime.strptime(fechaFinal, "%Y-%m-%d").date()
+        for gasto in listaGastos:
+            fecha_gasto = datetime.strptime(gasto["fecha"], "%Y-%m-%d").date()
+            if fechaInicio <= fecha_gasto <= fechaFinal:
+                gasto_filtrado = {k: gasto[k] for k in campos}
+                listaFecharango.append(gasto_filtrado)
+        if listaFecharango:
+            print(tabulate(listaFecharango, headers="keys", tablefmt="pipe"))
+        else:
+            print("No hay gastos que cumplan con la condición.")
+    elif(confirmacion==4):
+        print("Regresando al menú principal.........")
+    else:
+        print("\nOpcion no valida\nRegresando al menu principal.......\n")
+def mensaje3():
+    print("=============================================\
+            \n          Calcular Total de Gastos\
+            \n=============================================\
+            \nSeleccione el periodo de cálculo:\
+            \n\
+            \n1. Calcular total diario\
+            \n2. Calcular total semanal\
+            \n3. Calcular total mensual\
+            \n4. Regresar al menú principal\
+            \n============================================= ")
+def calculos(calculoOpcion,listaGastos):
+    if(calculoOpcion == 1 ):
+        totalDiario(listaGastos)
+    elif(calculoOpcion==2):
+        totalSemanal(listaGastos)
+    elif(calculoOpcion==3):
+        totalMes(listaGastos)
+    elif(calculoOpcion==4):
+        print("Regresando al menu principal")
+    else:
+        print("\nOpcion no valida\nRegresando al menu principal\n")
 
 def totalDiario(listaGastos):
     fechaActual=datetime.today().date()
@@ -43,37 +233,92 @@ def totalMes(listaGastos):
             gastoMensual.append(listaGastos[i])
             totalGastos +=listaGastos[i]["montoGasto"]
     if gastoMensual:
-        print(tabulate(gastoMensual, headers="keys", tablefmt="pipe"))
         print(f"\nTotal de gastos Mensual: {totalGastos}")
     else:
         print("No hay gastos registrados para el mes. ")
 def gastoDiario(totalGastos):
     print(f"\nTotal Gastos: ${totalGastos}")
 
+def mensaje4():
+    print("=============================================\
+            \n           Generar Reporte de Gastos\
+            \n=============================================\
+            \nSeleccione el tipo de reporte:\
+            \n\
+            \n1. Reporte diario\
+            \n2. Reporte semanal\
+            \n3. Reporte mensual\
+            \n4. Regresar al menú principal\
+            \n=============================================")
+
 def totalesDiarios(listaGastos):
     fechaActual=datetime.today().date()
     gastosDiario=[]
-    totalGastos=0
+    
+
     for i in range (len(listaGastos)):
         fechaGasto=datetime.strptime(listaGastos[i]["fecha"], "%Y-%m-%d").date()
         if (fechaGasto == fechaActual):
             gastosDiario.append(listaGastos[i])
-            listaTransporte=[]
-            listaComida=[]
-            campos=[]
-            if(listaGastos[i]["categoria"] == "comida"):
-                gasto = listaGastos[i]
-                gasto_filtrado = {k: gasto[k] for k in campos}
-                listaComida.append(gasto_filtrado)
-                totalGastos +=listaGastos[i]["montoGasto"]
-            if gastosDiario:
-                print(f"-Comida: {totalGastos}")
-            if (listaGastos[i]["categoria"] == "transporte"):
-                gasto = listaGastos[i]
-                totalGastos=0
-                gasto_filtrado = {k: gasto[k] for k in campos}
-                listaTransporte.append(gasto_filtrado)
-                totalGastos +=listaGastos[i]["montoGasto"]
-        if gastosDiario:
-            print(f"-Transporte: {totalGastos}")
+            reportes(listaGastos,i)
+    
 
+def reportes(listaGastos,i):
+
+    listaOtros=[]
+    listaHogar=[]
+    listasTecnologia=[]
+    listaRopa=[]
+    listaSalud=[]
+    listaEntretenimiento=[]
+    listaTransporte=[]
+    listaComida=[]
+    campos=[]
+    if(listaGastos[i]["categoria"] == "comida"):
+        gasto = listaGastos[i]
+        gasto_filtrado = {k: gasto[k] for k in campos}
+        listaComida.append(gasto_filtrado)
+        totalGastosComida +=listaGastos[i]["montoGasto"]
+    if (listaGastos[i]["categoria"] == "transporte"):
+        gasto = listaGastos[i]
+        gasto_filtrado = {k: gasto[k] for k in campos}
+        listaTransporte.append(gasto_filtrado)
+        totalGastosTransporte +=listaGastos[i]["montoGasto"]
+    if (listaGastos[i]["categoria"] == "entretenimiento"):
+        gasto = listaGastos[i]
+        gasto_filtrado = {k: gasto[k] for k in campos}
+        listaEntretenimiento.append(gasto_filtrado)
+        totalGastosEntretenimiento +=listaGastos[i]["montoGasto"]
+    if (listaGastos[i]["categoria"] == "salud"):
+        gasto = listaGastos[i]
+        gasto_filtrado = {k: gasto[k] for k in campos}
+        listaSalud.append(gasto_filtrado)
+        totalGastosSalud +=listaGastos[i]["montoGasto"]
+    if (listaGastos[i]["categoria"] == "ropa"):
+        gasto = listaGastos[i]
+        gasto_filtrado = {k: gasto[k] for k in campos}
+        listaRopa.append(gasto_filtrado)
+        totalGastosRopa +=listaGastos[i]["montoGasto"]
+    if (listaGastos[i]["categoria"] == "tecnologia"):
+        gasto = listaGastos[i]
+        gasto_filtrado = {k: gasto[k] for k in campos}
+        listasTecnologia.append(gasto_filtrado)
+        totalGastosTecnologia +=listaGastos[i]["montoGasto"]
+    if (listaGastos[i]["categoria"] == "hogar"):
+        gasto = listaGastos[i]
+        gasto_filtrado = {k: gasto[k] for k in campos}
+        listaHogar.append(gasto_filtrado)
+        totalGastosHogar +=listaGastos[i]["montoGasto"]
+    if (listaGastos[i]["categoria"] == "otros"):
+        gasto = listaGastos[i]
+        gasto_filtrado = {k: gasto[k] for k in campos}
+        listaOtros.append(gasto_filtrado)
+        totalGastosOtros +=listaGastos[i]["montoGasto"]
+print(f"-Comida: ${totalGastosComida}\
+    \n-Transporte: ${totalGastosTransporte}\
+    \n-Entretenimiento: ${totalGastosComida}\
+    \n-Salud: ${totalGastosSalud}\
+    \n-Ropa: ${totalGastosRopa}\
+    \n-Tecnologia: ${totalGastosTecnologia}\
+    \n-Hogar: ${totalGastosHogar}\
+    \n-Otros: ${totalGastosOtros}")
--- proyecto.py
+++ proyecto.py
@@ -1,6 +1,6 @@
 # ###################################
 # ##########Python-Proyecto##########
-# ###################################
+# ################################### 
 
 #Debemos crear un programa donde nos ayude a llevar nuestros gastos diarios 
 #Donde se puedan monitorear,modificar,mostrary eliminar
@@ -24,26 +24,12 @@ while(booleano):
     listaGastos=abrirJSON()
     #En este punto se actualiza la lista de gastos cada vez que se hace un cambio
     #CRUD (CREATE , READ , UPDATE & DELETE)
-    print("=============================================\
-    \n         Simulador de Gasto Diario\
-    \n=============================================\
-    \nSeleccione una opción:\
-    \n\
-    \n1. Registrar nuevo gasto\
-    \n2. Enumere los gastos\
-    \n3. Calcular total de gastos\
-    \n4. Generar reporte de gastos\
-    \n5. Salir\
-    \n=============================================")
+    mensajeIni()
     opcion=int(input(""))
     #Aqui es donde empezamos a saltar a las opciones que el usuario desea usar 
     #Opcion numero 1 es donde uno debe registrar los datos de los gastos nuevos
     if(opcion==1):
-        print("=============================================\
-            \n            Registrar Nuevo Gasto\
-        \n=============================================\
-        \nIngrese la informacion del gasto:\
-        \n")
+        mensaje1()
         monto=int(input("- Monto del gasto:  "))
         unidades=str(input("- Cantidad: "))
         clase=str(input("- Categoria ( comida, transporte, entretenimientos, otros):  "))
@@ -81,174 +67,15 @@ while(booleano):
         else:
             print("Gasto no ingresado")
     if(opcion==2):
-        print("=============================================\
-            \n                Listar Gastos\
-            \n=============================================\
-            \nSeleccione una opción para filtrar los gastos:\
-            \n""\
-            \n1. Ver todos los gastos\
-            \n2. Filtrar por categoria\
-            \n3. Filtrar por rango de fechas\
-            \n4. Regresar al menú principal\
-            \n=============================================")
+        mensaje2()
         confirmacion=int(input(""))
-        print("=============================================")
-        
-        if (confirmacion==1):
-            print(tabulate(listaGastos, tablefmt="double_outline"))
-        elif (confirmacion==2):
-            categoria=int(input("1. Comida\n2. Transporte\n3. Entretenimiento\n4. Salud\n5. Ropa\n6. Tecnologia\n7. Hogar\n8. Otros\n=============================================\n"))
-            if categoria == 1:
-                campos = ["fecha", "montoGasto", "categoria","cantidad"]
-                listaComida = []
-                for i in range(len(listaGastos)):
-                    if listaGastos[i]["categoria"] == "comida":
-                        gasto = listaGastos[i]
-                        gasto_filtrado = {k: gasto[k] for k in campos}
-                        listaComida.append(gasto_filtrado)
-                if listaComida:
-                    print(tabulate(listaComida, headers="keys", tablefmt="pipe"))
-                else:
-                    print("No hay gastos registrados para la clase de comida")
-            elif(categoria==2):
-                campos = ["fecha", "montoGasto", "categoria","cantidad"]
-                listaTransporte = []
-                for i in range(len(listaGastos)):
-                    if listaGastos[i]["categoria"] == "transporte":
-                        gasto = listaGastos[i]
-                        gasto_filtrado = {k: gasto[k] for k in campos}
-                        listaTransporte.append(gasto_filtrado)
-                if listaTransporte:
-                    print(tabulate(listaTransporte, headers="keys", tablefmt="pipe"))
-                else:
-                    print("No hay gastos registrados para la clase de transporte")
-            elif(categoria==3):
-                campos = ["fecha", "montoGasto", "categoria","cantidad"]
-                listaEntretenimiento = []
-                for i in range(len(listaGastos)):
-                    if listaGastos[i]["categoria"] == "entretenimiento":
-                        gasto = listaGastos[i]
-                        gasto_filtrado = {k: gasto[k] for k in campos}
-                        listaEntretenimiento.append(gasto_filtrado)
-                if listaEntretenimiento:
-                    print(tabulate(listaEntretenimiento, headers="keys", tablefmt="pipe"))
-                else:
-                    print("No hay gastos registrados para la clase de entretenimiento")
-            elif(categoria==4):
-                campos = ["fecha", "montoGasto", "categoria","cantidad"]
-                listaSalud = []
-                for i in range(len(listaGastos)):
-                    if listaGastos[i]["categoria"] == "salud":
-                        gasto = listaGastos[i]
-                        gasto_filtrado = {k: gasto[k] for k in campos}
-                        listaSalud.append(gasto_filtrado)
-                if listaSalud:
-                    print(tabulate(listaSalud, headers="keys", tablefmt="pipe"))
-                else:
-                    print("No hay gastos registrados para la clase de salud")
-            elif(categoria==5):
-                campos = ["fecha", "montoGasto", "categoria","cantidad"]
-                listaRopa = []
-                for i in range(len(listaGastos)):
-                    if listaGastos[i]["categoria"] == "ropa":
-                        gasto = listaGastos[i]
-                        gasto_filtrado = {k: gasto[k] for k in campos}
-                        listaRopa.append(gasto_filtrado)
-                if listaRopa:
-                    print(tabulate(listaRopa, headers="keys", tablefmt="pipe"))
-                else:
-                    print("No hay gastos registrados para la clase de ropa")
-            elif(categoria==6):
-                campos = ["fecha", "montoGasto", "categoria","cantidad"]
-                listaTecnología = []
-                for i in range(len(listaGastos)):
-                    if listaGastos[i]["categoria"] == "tecnología":
-                        gasto = listaGastos[i]
-                        gasto_filtrado = {k: gasto[k] for k in campos}
-                        listaTecnología.append(gasto_filtrado)
-                if listaTecnología:
-                    print(tabulate(listaTecnología, headers="keys", tablefmt="pipe"))
-                else:
-                    print("No hay gastos registrados para la clase de tecnología")
-            elif(categoria==7):
-                campos = ["fecha", "montoGasto", "categoria","cantidad"]
-                listaHogar = []
-                for i in range(len(listaGastos)):
-                    if listaGastos[i]["categoria"] == "hogar":
-                        gasto = listaGastos[i]
-                        gasto_filtrado = {k: gasto[k] for k in campos}
-                        listaHogar.append(gasto_filtrado)
-                if listaHogar:
-                    print(tabulate(listaHogar, headers="keys", tablefmt="pipe"))
-                else:
-                    print("No hay gastos registrados para la clase de hogar")
-            elif(categoria==8):
-                campos = ["fecha", "montoGasto", "categoria","cantidad"]
-                listaOtros = []
-                for i in range(len(listaGastos)):
-                    if listaGastos[i]["categoria"] == "otros":
-                        gasto = listaGastos[i]
-                        gasto_filtrado = {k: gasto[k] for k in campos}
-                        listaOtros.append(gasto_filtrado)
-                if listaOtros:
-                    print(tabulate(listaOtros, headers="keys", tablefmt="pipe"))
-                else:
-                    print("No hay gastos registrados para la clase de comida")
-            else:
-                print("=============================================\nNo se encontro opcion valida \nRegresando al menu principal")
-        elif(confirmacion==3):
-            campos=["fecha", "montoGasto", "categoria","descripcion","cantidad"]
-            listaFecharango=[]
-            fechaInicio=input("Ingrese la fecha inicial de la busqueda(formato YYYY-MM-DD): ")
-            fechaInicio = datetime.strptime(fechaInicio, "%Y-%m-%d").date()      
-            fechaFinal=input("Ingrese la fecha final de la busqueda (formato YYYY-MM-DD): ")
-            fechaFinal = datetime.strptime(fechaFinal, "%Y-%m-%d").date()
-            for gasto in listaGastos:
-                fecha_gasto = datetime.strptime(gasto["fecha"], "%Y-%m-%d").date()
-                if fechaInicio <= fecha_gasto <= fechaFinal:
-                    gasto_filtrado = {k: gasto[k] for k in campos}
-                    listaFecharango.append(gasto_filtrado)
-            if listaFecharango:
-                print(tabulate(listaFecharango, headers="keys", tablefmt="pipe"))
-            else:
-                print("No hay gastos que cumplan con la condición.")
-        elif(confirmacion==4):
-            print("Regresando al menú principal.........")
-        else:
-            print("\nOpcion no valida\nRegresando al menu principal.......\n")
+        filtroCate(confirmacion,listaGastos)
     if(opcion==3):
-        print("=============================================\
-            \n          Calcular Total de Gastos\
-            \n=============================================\
-            \nSeleccione el periodo de cálculo:\
-            \n\
-            \n1. Calcular total diario\
-            \n2. Calcular total semanal\
-            \n3. Calcular total mensual\
-            \n4. Regresar al menú principal\
-            \n============================================= ")
+        mensaje3()
         calculoOpcion=int(input(""))
-        if(calculoOpcion == 1 ):
-            totalDiario(listaGastos)
-        elif(calculoOpcion==2):
-            totalSemanal(listaGastos)
-        elif(calculoOpcion==3):
-            totalMes(listaGastos)
-        elif(calculoOpcion==4):
-            print("Regresando al menu principal")
-        else:
-            print("\nOpcion no valida\nRegresando al menu principal\n")
+        calculos(calculoOpcion,listaGastos)
     if(opcion==4):
-        print("=============================================\
-            \n           Generar Reporte de Gastos\
-            \n=============================================\
-            \nSeleccione el tipo de reporte:\
-            \n\
-            \n1. Reporte diario\
-            \n2. Reporte semanal\
-            \n3. Reporte mensual\
-            \n4. Regresar al menú principal\
-            \n=============================================")
+        mensaje4()
         opcion=int(input(""))
         if(opcion==1):
             x= str(date.today().strftime("%Y-%m-%d"))
@@ -256,7 +83,7 @@ while(booleano):
             print("Reporte Gastos - Dia: "+x)
             totalDiario(listaGastos)
             print("\n\nCategorias:")
-            totalesDiarios(listaGastos,json)
+            totalesDiarios(listaGastos)
         elif(opcion==2): 
             print("")
         elif(opcion==3):
@@ -268,6 +95,8 @@ while(booleano):
     elif(opcion==5):
         print("Gracias por usar el sistema de gestión de gastos. ¡Hasta la próxima!")
         booleano=False
+    else:
+        print("Opcion incorrecta")
     
 
 
